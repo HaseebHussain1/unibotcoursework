e097c9dd443a47b3d0b7958bd9fd4c86416c760d
Haseeb Hussain <haseeb091097@gmail.com>
2020-09-25 09:34:26 +0100

    created initial react frontend

---
+++ front end/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios'
+
+
+
+
+
+
+
+
+
+
+class App extends React.Component {
+
+  
+
+
+export default App;
