d849366cfce36ce89851ff44be365913c7d17c54
Haseeb Hussain <haseeb091097@gmail.com>
2021-02-22 10:29:19 +0100

    created ui to display all courses

--- front end/components/allCourses.js
+++ front end/components/allCourses.js
@@ -14,23 +14,83 @@ if edit is clicks redirects to course edit page
 */
 
 
-class allCourses extends React.Component {
+class Home extends React.Component {
 
   constructor(props){
+  super(props);
+  var sucsessmessage={istrue:false,message:""}
+  if(props.location.state){
+     sucsessmessage=props.location.state.message
+  }
+  this.state={
+    error:{istrue:false,message:""},
+    succsess:sucsessmessage,
+    courses:[]
+
+
+  }
+  console.log(this.props.location);
 
  }
  //{question:String,answer:String}
 
+ componentDidMount() {
 
 
 
 
+   setTimeout(() => {
+     const thisComp=this;
+     auth.currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+       console.log(idToken);
+
+
+     axios.get('https://us-central1-test-eixuyp.cloudfunctions.net/simple/courses',{ headers: {"Authorization" : `${idToken}`} })
+       .then(function (response) {
+         console.log(response);
+
+         thisComp.setState({ courses: response.data.courses });
+         console.log(thisComp.state.courses);
+       })
+})
+   }, 2000)}
+
+
+
+   scape=(event)=>{
+     auth.currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+       console.log(idToken);
+
+
+     axios.get('https://us-central1-test-eixuyp.cloudfunctions.net/simple/scrape',{ headers: {"Authorization" : `${idToken}`} })
+       .then(function (response) {
+
+       })
+   })}
   render() {
 
+    return (
+      <div className="bdy">
+      {lib.displayMessage(this.state.succsess)}
 
+         <div className="center_main">
+            <h1 className="centerTitle"> Courses</h1>
+            <input type="button" name="tags" value="scrape" onClick={(e) => this.scrape(e)}/>
+            {this.state.courses.map((course, index) => (
+            <div className="courseHolder">
+               <h2 className ="start">  course name</h2>
+               <p className ="start"> {course.course_name}</p>
+               <br/>
+               <br/>
+               <Link to={{pathname: '/course',query: { id: course._id }}} className="editBtn"> edit </Link>
+            </div>
+            ))}
+         </div>
+      </div>
+    );
   }
 }
 
 
 
-export default allCourses;
+export default Home;
