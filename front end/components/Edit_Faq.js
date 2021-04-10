ac8284192f8787224031cead5111b650a02c3fea
Haseeb Hussain <haseeb091097@gmail.com>
2021-04-10 14:25:19 +0100

    big update
    completed chatbot ui and user login system and fixed authentication

--- front end/components/Edit_Faq.js
+++ front end/components/Edit_Faq.js
@@ -12,13 +12,147 @@ and the update button triggeres a function which sends new data to node js
 
 class EditFaq extends React.Component {
   constructor(props){
+    super(props);
+    this.state={
+
+      id:"",
+      faq: {question:"",answer:""},
+      redirect:false,
+      error:{istrue:false,message:""},
+      succsess:{istrue:false,message:""},
+      url:"/faq"
+
+
+    }
+    if(this.props.location.query){
+      this.state.id=this.props.location.query.id;
+    }else{
+      console.log(this.state.redirect+" red");
+this.state.redirect=true;
+console.log(this.state.redirect+" red");
+
+}
+    console.log(this.state.id);
+
 
   }
+  componentDidMount() {
 
+
+
+    // Changing the state after 2 sec
+    // from the time when the component
+    // is rendered
+    setTimeout(() => {
+      const t=this;
+      auth.currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+        console.log(idToken);
+
+
+
+
+
+
+
+      axios.post('https://us-central1-test-eixuyp.cloudfunctions.net/simple/faq/edit',{id:t.state.id},{ headers: {"Authorization" : `${idToken}`} })
+      .then(function (response) {
+
+        t.setState({faq:response.data.faq})
+
+console.log(t.state.faq);
+      })
+    })
+
+    }, 2000)}
+    handleChange=(event,i)=>{
+      var faq=this.state.faq;
+if(event.target.name==="question"){
+  faq.question=event.target.value;
+}
+if(event.target.name==="answer"){
+  faq.answer=event.target.value;
+}
+      this.setState({faq:faq});
+
+
+    }
+
+
+
+
+  update=(event)=>{
+    this.setState({succsess:{istrue:false,message:""}});
+    this.setState({error:{istrue:false,message:""}});
+var context=this;
+
+if(true){
+  auth.currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+    console.log(idToken);
+
+
+
+
+
+
+
+axios.post('https://us-central1-test-eixuyp.cloudfunctions.net/simple/faq/edit/update',{id:context.state.id,faq:context.state.faq},{ headers: {"Authorization" : `${idToken}`} })
+.then(function (response) {
+
+context.setState({succsess:{istrue:true,message:"faq Updated"}})
+
+
+
+
+
+
+}).catch(e=>{
+  if(e.response.status==500) {
+    context.setState({error:{istrue:true,message:"server error"}})
+  }
+
+
+})
+})
+}
+  }
     //{question:String,answer:String}
     render() {
 
-  
+      var context=this;
+
+
+      return (
+        <div className="bdy">
+{lib.redirectWithmessage(this.state.redirect,this.state.redirectUrl,this.state.succsess)}
+
+        <div className="center_main">
+        {lib.displayMessage(this.state.succsess)}
+        <div>{lib.displayMessage(this.state.error)}</div>
+
+        <h1 className="centerTitle">Faq edit </h1>
+
+
+          <div className="faqElement">
+
+          question: <br/>
+          <textarea name="question" rows="5" cols="40" value= {this.state.faq.question} onChange={(e) => this.handleChange(e)}></textarea>
+        <br/>  answer: <br/>
+          <textarea name="answer" rows="5" cols="40" value= {this.state.faq.answer} onChange={(e) => this.handleChange(e)}></textarea>
+
+
+
+
+
+<br/>
+
+          <input type="button" name="update" value="update" onClick={(e) => this.update(e)}/>
+</div>
+        </div>
+
+</div>
+
+
+      );
     }
   }
 
--- front end/components/chatbot.js
+++ front end/components/chatbot.js
@@ -7,11 +7,134 @@ import './chatbot.css';
 
 class chatbot extends React.Component {
   constructor(props){
+  super(props);
+  var formattedMsg={
+  currentDateTime: Date().toLocaleString(),
+  isChatBot:true,
+  message:"Hi there, im becky your friendly university guide"
+
+  }
+  var formattedMsg2={
+  currentDateTime: Date().toLocaleString(),
+  isChatBot:true,
+  message:"How can i help you today?"
+
+  }
+var messages = [];
+messages.push(formattedMsg);
+
+messages.push(formattedMsg2);
+
+
+  this.state={
+  messages:messages,
+  currentMessage:""
+  }
+
+}
+ handleChange=(event)=>{
+   this.setState({currentMessage: event.target.value})
+}
+handleChatbotQueery=(event)=>{
+//ceck if enter
+// then make sure that http request to server. next
+
+if(event.key === 'Enter'){
+    event.preventDefault();
+
+// http
+var formattedMsg={
+  currentDateTime: Date().toLocaleString(),
+  isChatBot:false,
+  message:this.state.currentMessage
+
+}
+
+var querry= {
+      sessionId: '1',
+      queryInput: {
+        text: {
+            message: this.state.currentMessage,
+            languageCode: 'en-US'
+        }
+      }
+    }
+
+  this.setState(state=>{
+      const messages = [...this.state.messages, formattedMsg];// change to object
+
+    return{
+      messages,
+      message:''
+
+    }
+
+  });
+  var self=this;
+    axios.post('https://us-central1-test-eixuyp.cloudfunctions.net/dialogflowserver-app', querry)
+      .then(function (response) {
+        response.data.fulfillmentMessages.forEach((item, i) => {
+
+        var formattedMsg={
+        currentDateTime: Date().toLocaleString(),
+        isChatBot:true,
+        message:item.text.text
+
+        }
+        self.setState(state=>{
+          const messages = [...self.state.messages, formattedMsg];// change to object
+
+          return{
+            messages,
+            message:item.text.text
+
+          }
+
+        });
+      });
+
+
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+
+}
+
+
 
 }
 
 render() {
-
+  return (
+    <div className="bdy">
+     <div className="center_main">
+        <h1 className="centerTitle"> chatbot</h1>
+        <div class="chatbotbody">
+           <div className="chatbotName">
+              <h4 className="chatbotName">becky</h4>
+           </div>
+           <div class="messages">
+              <ul >
+                 {this.state.messages.map((tag, index) => (
+                 <li key={index}  className={` ${tag.isChatBot ? "chatbot" : "human"}`}>
+                 {tag.message}
+                 <br/>
+                 </li>
+                 ))}
+              </ul>
+           </div>
+           <input type="text" id="querry"
+              onChange={this.handleChange}
+              onKeyPress={(e) => this.handleChatbotQueery(e)}
+           />
+        </div>
+        <div >
+        </div>
+     </div>
+  </div>
+  );
 }
   }
 
